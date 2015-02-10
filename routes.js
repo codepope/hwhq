9b8d98925a347caac68a67d187eb8e7f69750fee
Dj Walker-Morgan <dj@Larry.local>
2015-02-10 21:48:16 +0000

    Now Adding....

--- routes.js
+++ routes.js
@@ -9,17 +9,20 @@ Router.map(function() {
   });
   this.route('ideas', {
     data: {
-      ideaList: function() { return Ideas.find() },
-      selectedIdea: { }
+      ideaList: function() {
+        return Ideas.find()
+      },
+      selectedIdea: {}
     }
-  }
-  );
+  });
   this.route('idea', {
     path: '/idea/:_id',
-    data: function () {
+    data: function() {
       return {
         ideaList: Ideas.find(),
-        selectedIdea: Ideas.findOne({_id: this.params._id})
+        selectedIdea: Ideas.findOne({
+          _id: this.params._id
+        })
       }
     },
     template: 'ideas'
@@ -40,4 +43,18 @@ if (Meteor.isClient) {
         template === currentRoute.lookupTemplate() ? 'active' : '';
     }
   });
+  Template.newIdea.events({
+    'submit form': function(evt) {
+      var name = evt.target.inputName.value;
+      var desc = evt.target.inputDescription.value;
+      Ideas.insert({
+        'name': name,
+        'description': desc,
+        proposer: Meteor.userId(),
+        created: Date.now()
+      });
+      console.log(name + ":" + desc);
+      return false;
+    }
+  })
 }
